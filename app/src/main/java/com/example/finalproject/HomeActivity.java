fea6302229442a425570b55a9d5ce0d114937b3a
Kat <pacek@uwindsor.ca>
2022-04-13 14:06:15 -0400

    Update Home Activity

--- app/src/main/java/com/example/finalproject/HomeActivity.java
+++ app/src/main/java/com/example/finalproject/HomeActivity.java
@@ -3,9 +3,13 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -68,7 +72,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        ption3Btn.setOnClickListener(new View.OnClickListener() {
+        option3Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (quizModelArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option3Btn.getText().toString().trim().toLowerCase())){
@@ -92,4 +96,43 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void showBottomSheet(){
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(HomeActivity.this);
+        View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.score_bottom_sheet,(LinearLayout)findViewById(R.id.idLLScore));
+        TextView scoreTV = bottomSheetView.findViewById(R.id.idTvScore);
+        Button restartQuizBtn = bottomSheetView.findViewById(R.id.idBtnRestart);
+        scoreTV.setText("Your score is\n"+currentScore + "/10"); //change if number of questions change
+
+        restartQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPos = random.nextInt(quizModelArrayList.size());
+                setDataToViews(currentPos);
+                questionsAttempted = 1;
+                currentScore = 0;
+                bottomSheetDialog.dismiss();
+            }
+        });
+
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+
+    }
+
+    private void setDataToViews(int currentPos){
+        questionNumberTV.setText("Questions Attempted: "+questionsAttempted + "/10" ); //change if number of questions change
+        if(questionsAttempted == 10){
+            showBottomSheet();
+        }
+        else{
+            questionTV.setText(quizModelArrayList.get(currentPos).getQuestion());
+            option1Btn.setText(quizModelArrayList.get(currentPos).getOption1());
+            option2Btn.setText(quizModelArrayList.get(currentPos).getOption2());
+            option3Btn.setText(quizModelArrayList.get(currentPos).getOption3());
+            option4Btn.setText(quizModelArrayList.get(currentPos).getOption4());
+        }
+    }
+
 }
