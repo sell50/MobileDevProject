f0a6f9e6662eb553f00a4279f0a6e1b916ab5997
Kat <pacek@uwindsor.ca>
2022-04-13 14:07:59 -0400

    Update MainActivity

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button register, signin;
-    DBHelper DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
