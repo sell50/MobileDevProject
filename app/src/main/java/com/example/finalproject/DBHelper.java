0b2c422dfb4cc9cccc486bd61569e5af9bae5075
FiRE-DRAG0N-X <sabell11@uwindsor.ca>
2022-04-13 16:28:02 -0400

    Create DBHelper.java
    - Uploaded by Julia Sabelli

---
+++ app/src/main/java/com/example/finalproject/DBHelper.java
@@ -0,0 +1,72 @@
+package com.example.loginapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME="login.db";
+    public DBHelper(Context context) {
+
+        super(context, "login.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table users(username TEXT primary key, password TEXT, fname TEXT, lname TEXT, email TEXT, birthdate TEXT, gender TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists users");
+    }
+
+    public Boolean insertData(String username, String password, String fname, String lname, String email, String dob, String gender) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("username", username);
+        values.put("password", password);
+        values.put("fname", fname);
+        values.put("lname", lname);
+        values.put("email", email);
+        values.put("birthdate", dob);
+        values.put("gender", gender);
+
+        long result = db.insert("users", null, values);
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    public Boolean checkUsername(String username) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from users where username=?", new String[] {username});
+        if(cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+
+    public Boolean checkEmailAddress(String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from users where email=?", new String[] {email});
+        if(cursor.getCount()>0) return true;
+        else return false;
+    }
+
+    public Boolean checkUsernamePassword(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from users where username=? and password=?", new String[] {username,password});
+        if(cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+}
