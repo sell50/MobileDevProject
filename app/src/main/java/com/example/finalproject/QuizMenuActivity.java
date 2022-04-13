6d6dd9fcd2ff1b19bb89b50e0b6bc3e66da65b89
Dariq A <72431760+DecodedSpark@users.noreply.github.com>
2022-04-13 19:25:17 -0400

    Update QuizMenuActivity.java

--- app/src/main/java/com/example/finalproject/QuizMenuActivity.java
+++ app/src/main/java/com/example/finalproject/QuizMenuActivity.java
@@ -4,21 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.bottomsheet.BottomSheetDialog;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 public class QuizMenuActivity extends AppCompatActivity {
 
-    private Button quiz1Btn, quiz2Btn, quiz3Btn;
+    Button quiz1Btn, quiz2Btn, quiz3Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +18,22 @@ public class QuizMenuActivity extends AppCompatActivity {
         quiz2Btn = findViewById(R.id.idQuizBtn2);
         quiz3Btn = findViewById(R.id.idQuizBtn3);
 
-        quiz1Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                intent.putExtra("QUIZ CHOICE",1);
-                startActivity(intent);
-            }
+        quiz1Btn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            intent.putExtra("QUIZ CHOICE",1);
+            startActivity(intent);
         });
 
-        quiz2Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                intent.putExtra("QUIZ CHOICE",2);
-                startActivity(intent);
-            }
-
+        quiz2Btn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            intent.putExtra("QUIZ CHOICE",2);
+            startActivity(intent);
         });
 
-        quiz3Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                intent.putExtra("QUIZ CHOICE",3);
-                startActivity(intent);
-            }
+        quiz3Btn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            intent.putExtra("QUIZ CHOICE",3);
+            startActivity(intent);
         });
     }
 }
