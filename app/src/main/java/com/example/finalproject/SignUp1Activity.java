6617cbb189dfe8cd636bbd6c7e9d13e7c32a482a
Dariq A <72431760+DecodedSpark@users.noreply.github.com>
2022-04-13 13:37:47 -0400

    Updated SignUp1

--- app/src/main/java/com/example/finalproject/SignUp1Activity.java
+++ app/src/main/java/com/example/finalproject/SignUp1Activity.java
@@ -3,12 +3,23 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp1Activity extends AppCompatActivity {
+
+    EditText username, password, repassword;
+    Button next, back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up1);
+
+        username= (EditText) findViewById(R.id.username);
+        password= (EditText) findViewById(R.id.password);
+        repassword= (EditText) findViewById(R.id.repassword);
+        next= (Button) findViewById(R.id.nextBtn1);
+        back= (Button) findViewById(R.id.backBtn1);
     }
 }
