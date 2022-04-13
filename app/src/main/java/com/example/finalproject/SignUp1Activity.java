015662793394609e1e1bfce7e240f022ddf3cf23
Dariq A <72431760+DecodedSpark@users.noreply.github.com>
2022-04-13 13:41:01 -0400

    Started button click functionality

--- app/src/main/java/com/example/finalproject/SignUp1Activity.java
+++ app/src/main/java/com/example/finalproject/SignUp1Activity.java
@@ -2,9 +2,13 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp1Activity extends AppCompatActivity {
 
@@ -21,5 +25,19 @@ public class SignUp1Activity extends AppCompatActivity {
         repassword= (EditText) findViewById(R.id.repassword);
         next= (Button) findViewById(R.id.nextBtn1);
         back= (Button) findViewById(R.id.backBtn1);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
