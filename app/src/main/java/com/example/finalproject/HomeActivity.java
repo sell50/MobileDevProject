d83edc56aa96b344d01871366f425a272f860da5
Kat <pacek@uwindsor.ca>
2022-04-13 14:01:20 -0400

    Update HomeActivity

--- app/src/main/java/com/example/finalproject/HomeActivity.java
+++ app/src/main/java/com/example/finalproject/HomeActivity.java
@@ -3,12 +3,35 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView questionTV, questionNumberTV;
+    private Button option1Btn, option2Btn, option3Btn, option4Btn;
+    private ArrayList<QuizModel> quizModelArrayList;
+    Random random;
+    int currentScore = 0, questionsAttempted = 1, currentPos, quizChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        questionTV = findViewById(R.id.idTVQuestion);
+        questionNumberTV = findViewById(R.id.idTVQuestionAttempted);
+        option1Btn = findViewById(R.id.idBtnOption1);
+        option2Btn = findViewById(R.id.idBtnOption2);
+        option3Btn = findViewById(R.id.idBtnOption3);
+        option4Btn = findViewById(R.id.idBtnOption4);
+        quizChoice = getIntent().getIntExtra("QUIZ CHOICE", 1);
+        quizModelArrayList = new ArrayList<>();
+        random = new Random();
+
+
     }
 }
